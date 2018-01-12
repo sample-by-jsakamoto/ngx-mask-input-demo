f617e5e4275937586037a20fc9a277cc25158f4b
jsakamoto <junichi.jp@gmail.com>
2018-01-12 13:09:55 +0900

    Initial Commit.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,27 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './components/app/app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import * as Material from '@angular/material';
-
 import '@angular/material/prebuilt-themes/indigo-pink.css';
+
 import './app.style.css';
+import { AppComponent } from './components/app/app.component';
 
 @NgModule({
     bootstrap: [AppComponent],
     imports: [
         BrowserModule,
-        HttpClientModule,
+        BrowserAnimationsModule,
         FormsModule,
         Material.MatToolbarModule,
         Material.MatCardModule,
         Material.MatButtonModule,
-        Material.MatRadioModule
+        Material.MatFormFieldModule,
+        Material.MatInputModule,
     ],
     declarations: [
-        AppComponent
+        AppComponent,
     ],
     providers: [
         { provide: 'BASE_URL', useFactory: getBaseUrl }
